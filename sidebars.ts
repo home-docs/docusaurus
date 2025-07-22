65d476ec85803d0d4827c5c8daf4665150ed13bf
prodigal4176 <prodigal4176@proton.me>
2025-07-22 14:05:49 +0900

    build error test fix

--- sidebars.ts
+++ sidebars.ts
@@ -98,11 +98,11 @@ const sidebars: SidebarsConfig = {
               type: 'category',
               label: 'Manage Azure identities and governance',
               items: [
-                {
-                  type: 'doc',
-                  id: 'notes/certification/az-104/manage_azure_identities_and_governance/create_users_and_groups',
-                  label: 'Create users and groups',
-                },
+                // {
+                //   type: 'doc',
+                //   id: 'notes/certification/az-104/manage_azure_identities_and_governance/create_users_and_groups',
+                //   label: 'Create users and groups',
+                // },
                 {
                   type: 'doc',
                   id: 'notes/certification/az-104/manage_azure_identities_and_governance/manage_user_and_group_properties',
